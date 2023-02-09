8cdb9f9c0c7098c993cdc4a3d032999232542da5
Atulya Kairati <atulyakairati@gmail.com>
2023-02-09 23:17:25 +0530

    Added recyclerview skeleton

--- app/src/main/java/com/atulya/photogallery/fragments/PhotoGalleryFragment.kt
+++ app/src/main/java/com/atulya/photogallery/fragments/PhotoGalleryFragment.kt
@@ -1,4 +1,39 @@
 package com.atulya.photogallery.fragments
 
-class PhotoGalleryFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.atulya.photogallery.databinding.FragmentPhotoGalleryBinding
+
+class PhotoGalleryFragment : Fragment() {
+
+    private var _binding: FragmentPhotoGalleryBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "binding is not initialized. Check is the view is visible"
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentPhotoGalleryBinding.inflate(inflater, container, false)
+
+        binding.photoGrid.layoutManager = GridLayoutManager(context, 3)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 }
